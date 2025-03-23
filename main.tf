4f66e41caf4f47ff0b0d517c1cb0e400ef00c293
regularpython <62233790+regularpython@users.noreply.github.com>
2025-03-23 18:16:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "sravan-regularpython"
-    key    = "my_lambda/terraform.tfstate"
+    key    = "my_lambda-2/terraform.tfstate"
     region = "us-east-1"
   }
 }
